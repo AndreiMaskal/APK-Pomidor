b065393a994df24e7b2643ee70e2eafcecd848df
Andrik <masmay.pl@gmail.com>
2022-05-20 23:37:47 +0200

    initial commit

---
+++ APK_Pomidor_Andrei_Maskal/APK_Pomidor_Andrei_Maskal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APK_Pomidor_Andrei_Maskal
+//
+//  Created by Andrei Maskal on 20/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
